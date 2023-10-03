99f3dc572286ef070011e2b1e89389d2674d30fa
Natnael Guchima <natnaelguchima@Natnaels-MacBook-Air.local>
2023-10-03 15:16:21 +0300

    configured vector icons for android

--- App/component/NavigationBar/index.jsx
+++ App/component/NavigationBar/index.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
+
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
 import colors from '../../config/colors';
 
 function NavigationBar({title, icon}) {
-  return <View style={styles.container}></View>;
+  return (
+    <View style={styles.container}>
+      <MaterialCommunityIcons name="menu" color={'red'} size={20} />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
